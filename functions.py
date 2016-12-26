e0c0ac21e858b39da95d9911555aa314132d9e7d
nyucel <necdetyucel@gmail.com>
2016-12-27 02:28:30 +0300

    bir sayıdan küçük bütün asal sayıları dizi olarak döndüren fonksiyon eklendi

--- functions.py
+++ functions.py
@@ -13,6 +13,18 @@ def isprime(a):
         i += 2
     return(prime)
 
+def allprimes(n):
+    """bir sayıdan küçük bütün asal sayıları dizi olarak döndüren fonksiyon"""
+    primes=[]
+    for i in range(2,n+1):
+        primes.append(i)
+    for x in range(0,int(n/2)+1):
+        if(primes[x]!=0):
+            for i in range(x+primes[x],n-1,primes[x]):
+                primes[i]=0
+    primes.sort()
+    return(primes[primes.count(0):])
+
 def divisors(n):
     """bir sayının bölenlerini bir dizi olarak geri döndüren fonksiyon"""
     carpanlar = [1]
