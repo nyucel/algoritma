c809c52da4950d1d68e67b75953ca4fb7cdedd91
nyucel <necdetyucel@gmail.com>
2016-12-28 22:58:03 +0300

    ozyinelemeli fonksiyon ornegi

---
+++ 505-BidirectionalRecurrence.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# özyinelemeli fonksiyon örneği olarak bakalım, sorunun çözümünü vermiyor bu hali :)
+
+def x(n):
+    if(n==0):
+        sonuc = 0
+    elif(n==1):
+        sonuc = 1
+    elif(n>1 and n%2==0):
+        sonuc = (3*x(int(n/2))+2*x(int(n/4)))%2**60
+    elif(n>1 and n%2==1):
+        sonuc = (2*x(int((n-1)/2))+3*x(int((n-1)/4)))%2**60
+    return(sonuc)
+
+def y(n,k):
+    if(k>=n):
+        sonuc = x(k)
+    else:
+        temp=[y(n,2*k),y(n,2*k+1)]
+        sonuc = 2**60-1-max(temp)
+    return(sonuc)
+
+def A(n):
+    return(y(n,1))
+
+print(A(10000))
