d5dffcf2d7a7c8fefbd8240b1c128daf613397ad
nyucel <necdetyucel@gmail.com>
2016-12-31 02:59:40 +0300

    isprime kullanılarak yeniden yazıldı

--- 10001stprime.py
+++ 10001stprime.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-#10001. asal sayı nedir?
-primes=[2,3]
-n=5
-while len(primes)<10001:
-    i=3
-    asalmi=0
-    while i<=n**(1/2):
-        if n%i==0:
-            asalmi=1
-        i=i+2
-    if asalmi==0:
-        primes.append(n)
-    n=n+2
-print(primes[10000])
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
+
+from functions import isprime
+
+n,p = 0,1
+while n<10001:
+    p += 1
+    if(isprime(p)==1):
+        n += 1
+print(p)
+
