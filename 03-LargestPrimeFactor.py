0b323c3d7364c48078fb7cacd595e86864d749e6
nyucel <necdetyucel@gmail.com>
2016-12-30 03:26:57 +0300

    What is the largest prime factor of the number 600851475143 ?

--- 03-LargestPrimeFactor.py
+++ 03-LargestPrimeFactor.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # What is the largest prime factor of the number 600851475143 ?
-from functions import isprime
-primes=[]
-number=600851475143
-for i in range(2,10000):
-    if isprime(i)==1:
-        primes.append(i)
-factor=[]
-for i in primes:
-    if number%i==0:
-        factor.append(i)
-print(factor[-1])
+from functions import allprimes
+
+primes=allprimes(10000)
+number,i=600851475143,1
+while not (number%primes[-i]==0):
+    i +=1
+print(primes[-i])
