891eb0c0ec08ba9a3fc039af05ce3500c364dd30
nyucel <necdetyucel@gmail.com>
2017-01-04 00:39:30 +0300

    Find the maximum total from top to bottom in triangle.txt, a 15K text file containing a triangle with one-hundred rows.

---
+++ 67-MaximumPathSumII.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Find the maximum total from top to bottom in triangle.txt, a 15K text file containing a triangle with one-hundred rows.
+
+dosya=open("p067_triangle.txt")
+triangle = []
+
+for line in dosya.readlines():
+    line = line.rstrip('\n').split(' ')
+    triangle.append(line)
+
+for n in range(len(triangle)-2,-1,-1):
+    for m in range(len(triangle[n])):
+        triangle[n][m] = int(triangle[n][m]) +  max(int(triangle[n+1][m]),int(triangle[n+1][m+1]))
+
+print(triangle[0][0])
