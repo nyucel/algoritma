043a90a5fef8ad54960acff595b9b98d906ac727
nyucel <necdetyucel@gmail.com>
2017-01-03 23:51:53 +0300

    Find the maximum total from top to bottom of the triangle

--- 104-PandigitalFibonacciEnds.py
+++ 104-PandigitalFibonacciEnds.py
@@ -9,6 +9,9 @@
 a, b, n = 1, 1, 2
 rakamlar = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+while n<329000:
+    a, b, n = b, a+b, n+1
+
 while not (sorted(str(b)[:9])==rakamlar and sorted(str(b)[-9:])==rakamlar):
     a, b, n = b, a+b, n+1
 print(n)
