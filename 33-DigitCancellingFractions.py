bae8e94c26bd9a8fa995ee6e80cec7e1c9635cde
nyucel <necdetyucel@gmail.com>
2017-01-06 00:54:02 +0300

    gereksiz b değişkeni çıkartıldı

--- 33-DigitCancellingFractions.py
+++ 33-DigitCancellingFractions.py
@@ -7,10 +7,9 @@
 
 sonuc = 1
 for a in range(11,100):
-    b=a%10
     if(a%10!=0):
         for n in range(1,10):
-            c=b*10+n
+            c=a%10*10+n
             if(c%11!=0):
                 if(a/c==int(a/10)/n):
                     sonuc *= c/a
