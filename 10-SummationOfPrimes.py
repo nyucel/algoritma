e9808cff38f98edc22aaa266b5eba05844d68916
nyucel <necdetyucel@gmail.com>
2017-01-13 18:14:17 +0300

    allprimes kullanılarak kısaltıldı

--- 10-SummationOfPrimes.py
+++ 10-SummationOfPrimes.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Find the sum of all the primes below two million.
-from functions import isprime
-toplam = 0
-for i in range(2,2000000):
-    if isprime(i)==1:
-        toplam += i
-print(toplam)
+from functions import allprimes
+
+print(sum(allprimes(2000000)))
