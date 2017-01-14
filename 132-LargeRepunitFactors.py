27d1f8466016dd73d449e87658b3bf5b01df12da
nyucel <necdetyucel@gmail.com>
2017-01-14 20:00:43 +0300

    Find the sum of the first forty prime factors of R(10^9)

--- 132-LargeRepunitFactors.py
+++ 132-LargeRepunitFactors.py
@@ -3,6 +3,9 @@
 # A number consisting entirely of ones is called a repunit. We shall define R(k) to be a repunit of length k.
 # For example, R(10) = 1111111111 = 11×41×271×9091, and the sum of these prime factors is 9414.
 # Find the sum of the first forty prime factors of R(10^9).
+# https://en.wikipedia.org/wiki/Repunit adresinden de görülebileceği gibi bu sayılar R(n)=10^n-1 şeklinde yazılabiliyor.
+# n asal sayısının (10^(10^9) - 1)'i bölmesi gerektiğini biliyoruz. Bu nedenle python'un pow fonksiyonunu kullanarak sonucu bulmak çok kolay oluyor.
+# pow kullanmak yerine 10**L%n yazmayı denediğinizde işlemlerin süresinin karşılaştırılamayacak kadar uzun sürdüğünü görebilirsiniz.
 
 from sympy import primerange
 
