f814ad8d4385dc86ac3c22bbfbefcb9cb5f65c95
nyucel <necdetyucel@gmail.com>
2017-11-09 07:59:23 +0300

    a hint is added

--- 030-DigitFifthPowers.py
+++ 030-DigitFifthPowers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+# hint: 6*9^5=354294
 toplam=0
 for i in range(1000,354294):
     control=0
