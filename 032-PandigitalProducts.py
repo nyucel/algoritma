bb98850898ec8ffc2ca04df53b0d577a24b68cf5
nyucel <necdetyucel@gmail.com>
2017-12-29 02:56:52 +0300

    liste yerine küme ile bir çözüm eklendi

--- 032-PandigitalProducts.py
+++ 032-PandigitalProducts.py
@@ -7,15 +7,10 @@
 
 from functions import ispandigital
 
-carpimlar=[]
+carpimlar=set()
 for a in range(1,500):
     for b in range(1,2000):
         q=''.join([str(a),str(b),str(a*b)])
         if(len(str(q))==9 and ispandigital(q)==1):
-            carpimlar.append(a*b)
-carpimlar.sort()
-
-for n in carpimlar:
-    while not (carpimlar.count(n)==1):
-        carpimlar.remove(n)
+            carpimlar.add(a*b)
 print(sum(carpimlar))
