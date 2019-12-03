5ee29014e5af97f42fd84425ea0c88373a6d7125
Necdet Yucel <necdetyucel@gmail.com>
2019-12-03 19:59:04 +0300

    daha uygun bir çözümle değiştirildi

--- 021-AmicableNumbers.py
+++ 021-AmicableNumbers.py
@@ -6,9 +6,23 @@
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 #Evaluate the sum of all the amicable numbers under 10000.
 
-from functions import divisors
-toplam=0
-for i in range(220,10000):
-    if(i!=sum(divisors(i)) and i==sum(divisors(sum(divisors(i))))):
-        toplam += i
-print(toplam)
+def d(n):
+    toplam = 1
+    q = int(n**(1/2)//1)+1
+    for p in range(2, q):
+        if(n%p==0):
+            toplam += p + n/p
+    if((q-1)**2==n):
+        return(int(toplam-q))
+    return(int(toplam))
+
+gtoplam = 0
+for m in range(2,10000):
+    s = d(m)
+    if(m==s):
+        continue
+    if(s>10000):
+        continue
+    if(m==d(s)):
+        gtoplam += m
+print(gtoplam)
