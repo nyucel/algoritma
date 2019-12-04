bf521814973f382d14c63d0f86a50b72e97c6dae
Necdet Yucel <necdetyucel@gmail.com>
2019-12-05 00:38:22 +0300

    daha hızlı bir çözümle değiştirildi

--- 012-HighlyDivisibleTriangularNumber.py
+++ 012-HighlyDivisibleTriangularNumber.py
@@ -1,12 +1,9 @@
 from functions import divisors
 
-def triangle(n):
-    sayi = 0
-    for i in range(1,n+1):
-        sayi += i
-    return(sayi)
+n, sayi = 1, 1
 
-n=1
-while len(divisors(triangle(n)))<500:
+while(len(divisors(sayi))<500):
     n += 1
-print(triangle(n))
+    sayi += n
+
+print(sayi)
