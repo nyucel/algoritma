c8753314eeca5ef1227ab63dbc441f31951d81fb
Necdet Yucel <necdetyucel@gmail.com>
2019-12-07 02:05:22 +0300

    daha uygun çözümle değiştirildi

--- 179-ConsecutivePositiveDivisors.py
+++ 179-ConsecutivePositiveDivisors.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Find the number of integers 1 < n < 107, for which n and n + 1 have the same number of positive divisors. For example, 14 has the positive divisors 1, 2, 7, 14 while 15 has 1, 3, 5, 15.
-# Burada c'yi 1'den başlatmamızın nedeni ardışık asal sayılar olan 2 ve 3'ü hesaba katmaktır.
 
-from sympy import divisor_count
+bolenler = [2]*(10**7+1)
+sayac = 0
 
-c = 1
-for n in range(2,10**7):
-    m=divisor_count(n)
-    if(m==2):
-        continue
-    if(m==divisor_count(n+1)):
-        c += 1
-print(c)
+for i in range(2, len(bolenler)//2):
+    for j in range(i*2, len(bolenler), i):
+        bolenler[j] += 1
+
+for i in range(2,len(bolenler)-1):
+    if(bolenler[i]==bolenler[i+1]):
+        sayac += 1
+
+print(sayac)
