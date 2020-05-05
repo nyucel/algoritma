329fdfa10ef344976b0d6f72657beb8b4579e8f6
Oguzhan İnce <oguzhan824@gmail.com>
2020-05-05 03:54:21 +0300

    Edited if-else statement with short form if
    Not really necessary, but I thought it increased the readability of the code n_n

--- 014-LongestCollatzSequence.py
+++ 014-LongestCollatzSequence.py
@@ -13,10 +13,8 @@ maks=[]
 for n in range(1,1000000):
     liste=[]
     while n!=1:
-        if n%2==0:
-            n=n/2
-        else:
-            n=3*n+1
+       # if-expression if (condition) else else-expression  
+        n = (n/2) if n%2==0 else 3*n+1
         liste.append(n)
     maks.append(len(liste)+1)
 print(maks.index(max(maks))+1)
