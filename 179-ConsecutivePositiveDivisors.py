d6f1e58e0f8396f9f499b922085c21d7ef843975
Necdet Yucel <necdetyucel@gmail.com>
2020-08-18 10:54:34 +0300

    Update 179-ConsecutivePositiveDivisors.py
    10 üzeri 7 ifadesi düzeltildi

--- 179-ConsecutivePositiveDivisors.py
+++ 179-ConsecutivePositiveDivisors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# Find the number of integers 1 < n < 107, for which n and n + 1 have the same number of positive divisors. For example, 14 has the positive divisors 1, 2, 7, 14 while 15 has 1, 3, 5, 15.
+# Find the number of integers 1 < n < 10^7, for which n and n + 1 have the same number of positive divisors.
 
 bolenler = [2]*(10**7+1)
 sayac = 0
