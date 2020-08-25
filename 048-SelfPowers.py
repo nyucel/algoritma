3d9005b86266e3dc760bd02b0618eea47415bd2d
Necdet Yucel <necdetyucel@gmail.com>
2020-08-25 04:33:46 +0300

    Update 048-SelfPowers.py
    değişken adı değiştirildi

--- 048-SelfPowers.py
+++ 048-SelfPowers.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
-sum=0
+toplam = 0
+
 for i in range(1,1001):
-    sum += i**i
-print(sum%(10**10))
+    toplam += i**i
+
+print(str(toplam)[-10:])
