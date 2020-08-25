829fcd256a2caaca565dbfe2cd21ec1d563ba526
Necdet Yucel <necdetyucel@gmail.com>
2020-08-26 00:23:56 +0300

    Update 023-Non-abundantSums.py

--- 023-Non-abundantSums.py
+++ 023-Non-abundantSums.py
@@ -4,7 +4,7 @@
 # A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
 # As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 # Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
-from fonksiyon import divisors
+from functions import divisors
 
 def list_diff(list1, list2):
         return (list(set(list1) - set(list2)))
