63814230af590e95ff8b00afafa6a07203800205
Necdet Yucel <necdetyucel@gmail.com>
2020-08-29 01:24:37 +0300

    Update 145-HowManyReversibleNumbersAreThereBelowOne-billion.py

--- 145-HowManyReversibleNumbersAreThereBelowOne-billion.py
+++ 145-HowManyReversibleNumbersAreThereBelowOne-billion.py
@@ -3,18 +3,18 @@
 # Some positive integers n have the property that the sum [ n + reverse(n) ] consists entirely of odd (decimal) digits. For instance, 36 + 63 = 99 and 409 + 904 = 1313. We will call such numbers reversible; so 36, 63, 409, and 904 are reversible. Leading zeroes are not allowed in either n or reverse(n).
 # There are 120 reversible numbers below one-thousand.
 # How many reversible numbers are there below one-billion (10^9)?
-# Çözümü yaklaşık 1 saatte bulabiliyor
+# Çözümü yaklaşık 20 dk'da bulabiliyor
 
 sayac = 0
+cift = ["0","2","4","6","8"]
 for n in range(1,10**9):
     if(n%10==0):
         continue
     reversen = n+int(str(n)[::-1])
     if(reversen%2==1):
-        for m in ["0","2","4","6","8"]:
-            if(list(str(reversen)).count(m)>0):
+        for m in cift:
+            if(m in list(str(reversen))):
                 break
         else:
             sayac += 1
 print(sayac)
-
