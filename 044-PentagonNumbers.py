1c30e3a24af933fe20ad3a1f0ed61b2286df11bf
Necdet Yucel <necdetyucel@gmail.com>
2020-11-08 00:23:45 +0300

    Update 044-PentagonNumbers.py

--- 044-PentagonNumbers.py
+++ 044-PentagonNumbers.py
@@ -9,11 +9,11 @@ def P(n):
     return(int(n*(3*n-1)/2))
 
 pentagons = []
-for n in range(1,10000):
+for n in range(1,3000):
     pentagons.append(P(n))
-
+    
 for p in pentagons:
     for q in pentagons[(pentagons.index(p)+1):]:
-        if(pentagons.count(p+q)==1):
-            if(pentagons.count(q-p)==1):
+        if((p+q) in pentagons):
+            if((q-p) in pentagons):
                 print(q-p)
