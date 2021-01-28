b6c075cd7563578481c04acec662c3ad0e5d5041
Necdet Yucel <necdetyucel@gmail.com>
2021-01-28 10:10:13 +0300

    liste küçültüldü

--- 092-SquareDigitChains.py
+++ 092-SquareDigitChains.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #7 basamaklı en büyük sayı 9999999. onun da basamaklarındaki rakamların karelerinin toplamı 7*9^2=567
+#1'e ulaşan sayıların sayısı çok daha az olduğundan onların sayısını bulup 10 milyondan çıkartmak daha küçük bir listeyle çalışmaya imkan verir.
 
 def karetoplam(n):
     toplam = 0
@@ -9,19 +10,19 @@ def karetoplam(n):
         n = n//10
     return(toplam)
 
-seksendokuz = set()
+birler = {1}
 
 for p in range (2,568):
     x = p
     while(x!=1 and x!=89):
         x = karetoplam(x)
-    if(x==89):
-        seksendokuz.add(p)
+    if(x==1):
+        birler.add(p)
 
-cozum = len(seksendokuz)
+cozum = len(birler)
 
 for n in range(568,10000000):
-    if(karetoplam(n) in seksendokuz):
+    if(karetoplam(n) in birler):
         cozum += 1
 
-print(cozum)
+print(9999999-cozum)
