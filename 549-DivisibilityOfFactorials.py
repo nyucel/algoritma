b69740c24ca952d59d9134ea9bcad1d2c23d30dd
Necdet Yucel <necdetyucel@gmail.com>
2021-03-13 20:05:52 +0300

    549. sorunun çözümü eklendi

---
+++ 549-DivisibilityOfFactorials.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+LIMIT = 10**8
+enkucuk = [0] * (LIMIT + 1)
+
+for i in range(2, len(enkucuk)):
+    if enkucuk[i] == 0:
+        kuvvet = 1
+        j = i
+        while(kuvvet<LIMIT):
+            kuvvet *= i
+            for k in range(kuvvet, len(enkucuk), kuvvet):
+                enkucuk[k] = max(j, enkucuk[k])
+            temp = j // i
+            while temp % i == 0:
+                kuvvet *= i
+                temp //= i
+            j += i
+
+print(sum(enkucuk))
